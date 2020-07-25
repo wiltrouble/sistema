451a7d59c3199dc410e918a24f54e11a9df56efc
Wilson Lopez <wiltrouble@gmail.com>
2020-07-25 03:48:50 -0400

    Initial configuration

---
+++ resources/views/content/content.blade.php
@@ -0,0 +1,57 @@
+@extends('main')
+@section('content')
+        <template v-if="menu==0">
+            <example-component></example-component>
+        </template>
+
+        <template v-if="menu==1">
+            <h1>Content of menu 1</h1>
+        </template>
+
+        <template v-if="menu==2">
+            <h1>Content of menu 2</h1>
+        </template>
+
+        <template v-if="menu==3">
+            <h1>Content of menu 3</h1>
+        </template>
+
+        <template v-if="menu==4">
+            <h1>Content of menu 4</h1>
+        </template>
+
+        <template v-if="menu==5">
+            <h1>Content of menu 5</h1>
+        </template>
+
+        <template v-if="menu==6">
+            <h1>Content of menu 6</h1>
+        </template>
+
+        <template v-if="menu==7">
+            <h1>Content of menu 7</h1>
+        </template>
+
+        <template v-if="menu==8">
+            <h1>Content of menu 8</h1>
+        </template>
+
+        <template v-if="menu==9">
+            <h1>Content of menu 9</h1>
+        </template>
+
+        <template v-if="menu==10">
+            <h1>Content of menu 10</h1>
+        </template>
+
+        <template v-if="menu==11">
+            <h1>Content of menu 11</h1>
+        </template>
+
+        <template v-if="menu==12">
+            <h1>Content of menu 12</h1>
+        </template>
+
+
+        
+@endsection        
